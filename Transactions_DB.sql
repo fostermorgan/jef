ba8104d52a4d707bb69b6ff9e94b1adf57cf3501
Jake Oliversen <jake.oliversen@gmail.com>
2019-07-26 10:58:38 -0500

    WIP: All tables in transactions added

--- Transactions_DB.sql
+++ Transactions_DB.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS transactions(
   transactionID INT AUTO_INCREMENT,
-  transactions_to_catgories_ID INT,
+  transactions_to_categories_ID INT,
   payeeID INT,
   userID INT,
   typeID INT,
@@ -20,4 +20,32 @@ CREATE TABLE IF NOT EXISTS users(
   email VARCHAR(256),
   password VARCHAR(256),
   role VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS payees(
+  payeeID INT AUTO_INCREMENT,
+  name VARCHAR(256),
+  address VARCHAR(256),
+  city VARCHAR(256),
+  zip VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS transactions_to_categories(
+  transactions_to_categories_ID INT AUTO_INCREMENT,
+  transactionsID INT,
+  categoryID INT,
+  classID INT,
+  discription VARCHAR(256),
+  amount DECIMAL(13, 2),
+);
+
+CREATE TABLE IF NOT EXISTS categories(
+  categoryID INT AUTO_INCREMENT,
+  categoryName VARCHAR(100)
+);
+
+
+CREATE TABLE IF NOT EXISTS classes(
+  classID INT AUTO_INCREMENT,
+  name varchar(256)
 );
